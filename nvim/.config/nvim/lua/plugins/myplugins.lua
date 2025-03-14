e2beeedc21c972cdf439580ec0f8a9ffb3fd2ca1
CodesWithRobi <rayercodes@gmail.com>
2025-03-14 10:44:02 +0530

    fixed dap

--- nvim/.config/nvim/lua/plugins/myplugins.lua
+++ nvim/.config/nvim/lua/plugins/myplugins.lua
@@ -94,11 +94,11 @@ local plugins = {
 
   {
     "mfussenegger/nvim-dap",
-    -- config = function()
-    --   require "configs.dap"
+    config = function()
+      require "configs.dap"
     --   map("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", { desc = "Add breakpoint at line" })
     --   map("n", "<leader>dr", "<cmd>DapContinue<CR>", { desc = "Run or continue the debugger" })
-    -- end
+    end
   },
 
   { "nvim-neotest/nvim-nio" },
