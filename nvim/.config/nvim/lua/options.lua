7e47e4b032da5eb19e47d1a4ab5cc56ac111b572
CodesWithRobi <rayercodes@gmail.com>
2026-05-14 10:57:49 +0530

    configured netrw

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,6 +1,9 @@
 require "nvchad.options"
 
 -- add yours here!
-
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 75
